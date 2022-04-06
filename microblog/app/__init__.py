a17e2667717acb035701e30d638327fdc92c214d
architux <architux@protonmail.com>
2022-04-06 17:48:02 +0300

    feat: Add basic database support

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from config import Config
 
@@ -6,4 +8,7 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
